4b7ffddbfc0c631ba9ed6dbfaf8935f8f1c48d15
wmd <daiyvquan@126.com>
2020-02-16 02:07:23 +0800

    加了个文本框

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <hexagon v-for="Pos in this.gamemap.hexagon" :key="Pos.label" :P="Pos"/>
     <node v-for="Pos in this.gamemap.node" :key="Pos.label" :P1="Pos.N0" :P2="Pos.N1" :P3="Pos.N2"/>
     <road v-for="Pos in this.gamemap.road" :key="Pos.label" :P1="Pos.N0" :P2="Pos.N1"/>
-    <road/>
+    <textarea rows="10" cols="50" readonly="readonly" style="position:absolute;left:1300px;top:500px;resize:none;font-size:16px;" v-model="showmsg"/>
+    
   </div>
 </template>
 
@@ -23,11 +24,27 @@ export default {
   },
   data:function(){
     return{
-      gamemap:0
+      gamemap:0,
+      showmsg:"卡坦岛内测版0.1"//用于显示游戏界面文字提示
     }
   },
   beforeMount(){
-      this.gamemap=gamecalc.initMap();
+    var webSocket = new WebSocket("ws://localhost:2333");
+    webSocket.onerror = function (event){
+      alert('无法获得与服务器的连接，请联系管理员？'+String(event));
+    };
+    /*
+    // 打开websocket
+    webSocket.onopen = function (event){
+      onOpen(event);
+    };  
+
+    //监听消息
+    webSocket.onmessage = function (event){
+      onMessage(event);
+    };
+    */
+    this.gamemap=gamecalc.initMap();
   }
 }
 </script>
