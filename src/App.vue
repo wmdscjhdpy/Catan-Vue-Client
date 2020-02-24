529fa63669cff13b7c4b807fb32c3d8a3ce937a1
wmd <daiyvquan@126.com>
2020-02-24 17:02:56 +0800

    为界面添加骰子

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <hexagon v-for="hexa in gamemap.hexagon" :key="hexa.label" :P="hexa.Pos" :rollnum="hexa.number" :kind="hexa.kind"/>
       <node v-for="nod in gamemap.node" :key="nod.label" :P1="nod.Pos[0]" :P2="nod.Pos[1]" :P3="nod.Pos[2]"/>
       <road v-for="roa in gamemap.road" :key="roa.label" :P1="roa.Pos[0]" :P2="roa.Pos[1]"/>
+      <roll :num="Array(3,4)" style="position:absolute;left:1000px;top:50px"/>
     </div>
     <div v-if="gamemap==null">
       <img :src="backgroundimg" width="900" height="900" style="position:absolute;left:100px;top:20px;"/>
@@ -20,6 +21,7 @@ import node from './components/node.vue'
 import road from './components/road.vue'
 import gamecalc from './components/gamecalc.js'
 import room from './components/room.vue'
+import roll from './components/roll.vue'
 
 export default {
   name: 'App',
@@ -28,11 +30,13 @@ export default {
     node,
     road,
     room,
+    roll
   },
   data:function(){
     return{
       gamemap:null,
-      backgroundimg:background
+      backgroundimg:background,
+
     }
   },
   beforeMount(){
---
+++ src/components/roll.vue
@@ -0,0 +1,38 @@
+<template>
+    <div style="cursor:pointer;" @click="rolling()">
+        <img :src="roll[num[0]]" height="100" width="100" style="position:absolute;left:0px;top:0px;"/>
+        <img :src="roll[num[1]]" height="100" width="100" style="position:absolute;left:120px;top:0px;"/>
+    </div>
+</template>
+
+<script>
+import roll0 from '../assets/roll/0.jpg'
+import roll1 from '../assets/roll/1.jpg'
+import roll2 from '../assets/roll/2.jpg'
+import roll3 from '../assets/roll/3.jpg'
+import roll4 from '../assets/roll/4.jpg'
+import roll5 from '../assets/roll/5.jpg'
+import roll6 from '../assets/roll/6.jpg'
+
+export default {
+    props:['num'],
+    data:function(){
+        return{
+            roll:{
+                0:roll0,
+                1:roll1,
+                2:roll2,
+                3:roll3,
+                4:roll4,
+                5:roll5,
+                6:roll6
+            }
+        }
+    },
+    methods:{
+        rolling(){
+            alert('rolling');
+        }
+    }
+}
+</script>
